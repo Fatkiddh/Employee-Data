2d18bd5e2d7b82f59179c756a5f9fbff121a75a1
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-01-10 19:04:15 -0800

    folders frameware

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+var config = {
+    apiKey: "AIzaSyCdfiV0EJIkGNK-EGeTnW531q080uA3Zc8",
+    authDomain: "employee-data-d940a.firebaseapp.com",
+    databaseURL: "https://employee-data-d940a.firebaseio.com",
+    projectId: "employee-data-d940a",
+    storageBucket: "employee-data-d940a.appspot.com",
+    messagingSenderId: "560173070614"
+  };
+
+firebase.initializeApp(config);
